4dfc9d2a6ce560f8190a40ff9d2b0ae6fcaaf989
Simon <simon.whittemore@gmail.com>
2019-11-12 22:28:27 +0000

    Updates

---
+++ precache-manifest.80d262f558669b7dc93604a5ad3670fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82365ff1742b712640a655673ac9f57a",
+    "url": "/adjective-declensions/index.html"
+  },
+  {
+    "revision": "ec0ca67349d07951347f",
+    "url": "/adjective-declensions/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ee29000262dfdbc452b",
+    "url": "/adjective-declensions/static/js/2.d928bc07.chunk.js"
+  },
+  {
+    "revision": "ec0ca67349d07951347f",
+    "url": "/adjective-declensions/static/js/main.22aae542.chunk.js"
+  },
+  {
+    "revision": "c85e6763a40d48b13cdb",
+    "url": "/adjective-declensions/static/js/runtime-main.c7c3fb44.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/adjective-declensions/static/media/logo.25bf045c.svg"
+  }
+]);
